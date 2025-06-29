dab6d3259bb077ce5945c26d3f43ed293361307c
Pablo Esteban Pedroza Bernal <reptitalz@gmail.com>
2025-06-29 03:33:53 +0000

    quiero que el modo dev se vea igual que en el modo prod. no puedo visual

--- code/src/app/dashboard/page.tsx
+++
@@ -1,5 +0,0 @@
-// This file created a conflicting route at /dashboard and caused build errors
-// because it was being imported as a component into another page.
-// Its logic has been consolidated into /src/components/dashboard/ConsumptionViewDialog.tsx
-// and this file is now intentionally left blank to disable the conflicting route.
-// It can be safely deleted.
