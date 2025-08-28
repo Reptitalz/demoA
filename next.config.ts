4d1fbbdbbed92f4f366f742552c1059c468f6bc1
Pablo Esteban Pedroza Bernal <reptitalz@gmail.com>
2025-08-28 17:27:09 +0000

    seguimos teniendo problema con el randobyte root@6924845c5440:/code# npm

--- next.config.ts
+++ next.config.ts
@@ -38,12 +38,6 @@ const securityHeaders = [
   },
 ];
 
-const withPWA = require('@ducanh2912/next-pwa').default({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-  register: true,
-  skipWaiting: true,
-});
 
 const nextConfig = {
   env: {
@@ -114,4 +108,4 @@ const nextConfig = {
   },
 };
 
-module.exports = withPWA(nextConfig);
+module.exports = nextConfig;
