f296c4f55f0f2bc8c3506df144684d8e6ce992ef
Pablo Esteban Pedroza Bernal <reptitalz@gmail.com>
2025-09-17 22:12:25 +0000

    esta algo lento dentro de aqui, haz que no se trabe (_for element div_)

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -6,18 +6,14 @@ import {cn} from '@/lib/utils';
 const Textarea = React.forwardRef<HTMLTextAreaElement, React.ComponentProps<'textarea'>>(
   ({className, ...props}, ref) => {
     return (
-        <div className={cn("relative p-0.5 rounded-lg transition-all", 
-          "bg-brand-gradient shiny-border"
-        )}>
-            <textarea
-                className={cn(
-                'relative flex min-h-[80px] w-full rounded-md border-0 bg-card px-3 py-2 text-base ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
-                className
-                )}
-                ref={ref}
-                {...props}
-            />
-       </div>
+      <textarea
+        className={cn(
+          'flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
+          className
+        )}
+        ref={ref}
+        {...props}
+      />
     );
   }
 );
