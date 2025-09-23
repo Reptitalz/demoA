d466f8dcab85a6143fa3663b44aa1d06b787bd7e
Pablo Esteban Pedroza Bernal <reptitalz@gmail.com>
2025-09-23 18:36:00 +0000

    usa el 2.svg pero no lo modifiques (_for element img_)

--- src/components/shared/AppIcon.tsx
+++ src/components/shared/AppIcon.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 
 const AppIcon = ({ className, ...props }: Omit<React.ComponentProps<typeof Image>, 'src' | 'alt'>) => (
     <Image 
-        src="/heymanito.svg"
+        src="/2.svg"
         alt="Hey Manito Icon"
         width={100}
         height={100}
