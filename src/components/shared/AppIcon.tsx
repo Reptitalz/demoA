e4cba50184341d9fdc6ba851437048d496c43284
Pablo Esteban Pedroza Bernal <reptitalz@gmail.com>
2025-09-23 15:36:48 +0000

    coloca otra vez icon.svg de public (_for element <AppIcon>_)

--- src/components/shared/AppIcon.tsx
+++ src/components/shared/AppIcon.tsx
@@ -1,38 +1,27 @@
 "use client";
 
 import { cn } from '@/lib/utils';
-import type { SVGProps } from 'react';
+import Image from 'next/image';
+import type { ComponentProps } from 'react';
 
-const AppIcon = ({ className, ...props }: SVGProps<SVGSVGElement>) => (
-    <svg
-        viewBox="0 0 100 100"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
+// Using Omit to exclude props that will be explicitly set.
+// This provides better type safety than just accepting SVGProps.
+type AppIconProps = Omit<ComponentProps<typeof Image>, 'src' | 'alt' | 'width' | 'height'> & {
+    className?: string;
+    width?: number;
+    height?: number;
+};
+
+const AppIcon = ({ className, width = 100, height = 100, ...props }: AppIconProps) => (
+    <Image 
+        src="/icon.svg"
+        alt="Hey Manito App Icon"
+        width={width}
+        height={height}
         className={cn('h-auto w-full', className)}
+        // The unoptimized prop is not needed for local static assets like this.
         {...props}
-    >
-        <path
-            d="M50 0C22.3858 0 0 22.3858 0 50C0 77.6142 22.3858 100 50 100C77.6142 100 100 77.6142 100 50C100 22.3858 77.6142 0 50 0Z"
-            fill="url(#paint0_linear_1_2)"
-        />
-        <path
-            d="M39.63 70.08L39.52 32.55C39.52 30.56 40.85 29.58 42.41 30.34L73.18 45.4C74.65 46.12 74.62 47.38 73.14 48.07L42.52 69.46C40.88 70.28 39.63 69.24 39.63 70.08Z"
-            fill="white"
-        />
-        <defs>
-            <linearGradient
-                id="paint0_linear_1_2"
-                x1="0"
-                y1="0"
-                x2="100"
-                y2="100"
-                gradientUnits="userSpaceOnUse"
-            >
-                <stop stopColor="#8A2BE2" />
-                <stop offset="1" stopColor="#FF1493" />
-            </linearGradient>
-        </defs>
-    </svg>
+    />
 );
 
 export default AppIcon;
