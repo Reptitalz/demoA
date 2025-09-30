e0f917e43fc864930cdd5b2da160411fffe96758
Pablo Esteban Pedroza Bernal <reptitalz@gmail.com>
2025-09-30 05:44:06 +0000

    Try fixing this error: `Unhandled Runtime Error: Error: Label is not def

--- src/components/chat/admin/InstructionsDialog.tsx
+++ src/components/chat/admin/InstructionsDialog.tsx
@@ -13,6 +13,7 @@ import { Card, CardContent } from '@/components/ui/card';
 import { cn } from '@/lib/utils';
 import { ShoppingCart, HandCoins, Handshake, LifeBuoy, ClipboardList, CheckCircle } from 'lucide-react';
 import { ScrollArea } from '@/components/ui/scroll-area';
+import { Label } from '@/components/ui/label';
 
 
 interface InstructionsDialogProps {
